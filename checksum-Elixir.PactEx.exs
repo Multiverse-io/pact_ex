660ba6f3e0e84aa85de6ce5acdacdd4614937ad0
rohan-b99 <43239788+rohan-b99@users.noreply.github.com>
2024-10-18 15:27:23 +0100

    v0.3.2

---
+++ checksum-Elixir.PactEx.exs
@@ -0,0 +1,5 @@
+%{
+  "libpact_ex-v0.3.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:feb4e05c2d4c37fa303450350a850257ea88845e9967fa473e76307c7a0853d6",
+  "libpact_ex-v0.3.2-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:d666af01ba276531cf6d252b019a9187c6fadf3e9ffd7e5501ecf01df46b28b4",
+  "libpact_ex-v0.3.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:1f9a65f18f8adc30a61a68a6735a9396568bc7e183111dd1a3b10c0b8dd7941e",
+}
